e7037131f2be7b339cdbc8a8502bccb727625acc
Ali Faal <faal.ali.faal@gmail.com>
2024-12-21 14:53:55 +0330

    adding poppins font to our project

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,11 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,7 +27,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} ${poppins.className}`}>
         {children}
       </body>
     </html>
