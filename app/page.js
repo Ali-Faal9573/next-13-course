6b3402d9eb270c3cc3015e84dcc5412774ef0b11
Ali Faal <faal.ali.faal@gmail.com>
2024-12-21 11:39:41 +0330

    adding links to home page.

--- app/page.js
+++ app/page.js
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import Link from 'next/link'
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <h1>Welcome To Traversy Media</h1>
+      <ul>
+        <li><Link href='/'>Home</Link></li>
+        <li><Link href='/about'>About</Link></li>
+        <li><Link href='/about/team'>Team</Link></li>
+      </ul>
+    </div>
   )
 }
 
