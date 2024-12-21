ae6f9aa201c527417f9c1a4f17479b81b0b6f0f3
Ali Faal <faal.ali.faal@gmail.com>
2024-12-21 11:42:40 +0330

    adding some dummy data to about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const AboutPage = () => {
     return (
-        <div>AboutPage</div>
+        <div>
+            <h1>About Traversy Media</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, vel. Maxime eius libero ab incidunt perferendis, minima sapiente cumque culpa. Dolorum iusto consectetur repellendus mollitia quas expedita rem soluta iste et. Ducimus quis laborum qui modi quas rerum praesentium maiores fugit quidem. Facilis optio a placeat officia sint libero molestiae?</p>
+        </div>
     )
 }
 
